2770cf07a93d471a4eb436d6e033fabf658cf219
Pangpangju <joo90319@gmail.com>
2023-12-02 00:13:15 +0900

    Lobby랑 Relay 연동 성공

--- Assets/Scripts/Lobby/LobbyManager.cs
+++ Assets/Scripts/Lobby/LobbyManager.cs
@@ -11,7 +11,6 @@ using Unity.Networking.Transport.Relay;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using Unity.Services.Relay.Models;
-using Unity.Services.Core;
 
 
 
@@ -25,7 +24,6 @@ public class LobbyManager : NetworkBehaviour
     //void RefreshLobby()   방에 변동사항(입장, 퇴장) 생기면 최신화
 
     #region 1.변수들 
-    private Lobby hostLobby;
     private Lobby joinedLobby;
 
 
@@ -38,15 +36,15 @@ public class LobbyManager : NetworkBehaviour
     [SerializeField] GameObject playerInRoomPrefab;
     [SerializeField] Button leaveRoomButton;
     [SerializeField] GameObject roomPanel;
-
+    [SerializeField] Button startGameButton;
 
     private float heartbeatTimer;
     private float lobbyUpdateTimer;
 
-    
+    private bool isLobbyHost;
 
     #endregion
-    #region 2. Start, Update
+    #region 2.Start, Update
     private void Update(){
         HandleLobbyHeartbeat();
         HandleLobbyPollForUpdates();
@@ -65,12 +63,15 @@ public class LobbyManager : NetworkBehaviour
             CreateLobbyOptions createLobbyOptions = new CreateLobbyOptions      //나중에 여기다가 옵션 추가할 수 있음
             {
                 IsPrivate = false,
-                Player = GetPlayer()
+                Player = GetPlayer(),
+                Data = new Dictionary<string, DataObject> {
+                    { "START_GAME", new DataObject(DataObject.VisibilityOptions.Member, "0")}       //게임 시작을 이걸로 해둠
+                }
             };
 
             Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers_input.value+2, createLobbyOptions);
-            hostLobby = lobby;
-            joinedLobby = hostLobby;
+            isLobbyHost = true;
+            joinedLobby = lobby;
 
             var callbacks = new LobbyEventCallbacks();
             callbacks.LobbyChanged += OnLobbyChanged;
@@ -78,6 +79,8 @@ public class LobbyManager : NetworkBehaviour
             await Lobbies.Instance.SubscribeToLobbyEventsAsync(lobby.Id, callbacks);
             Debug.Log("Created Lobby! " + lobby.Name + " " + lobby.MaxPlayers + " " + lobby.LobbyCode);
             roomPanel.SetActive(true);
+
+
 
             GameObject playerPanel = Instantiate(playerInRoomPrefab, playerHolder);
             PlayerCreated playerCreated = playerPanel.GetComponent<PlayerCreated>();        //방만들때는 이벤트 아직 못받아서 직접 Panel 만들어줘야함 그 후로는 자동
@@ -93,7 +96,7 @@ public class LobbyManager : NetworkBehaviour
 
     }
     [Command]
-    public async void RefreshLobby() {
+    public async void RefreshLobby() {          //로비 목록을 Query에서 받아와 최신화 시켜줌
         try { 
             QueryResponse queryResponse = await Lobbies.Instance.QueryLobbiesAsync();
             Debug.Log("Lobbies Found: " + queryResponse.Results.Count);
@@ -115,8 +118,8 @@ public class LobbyManager : NetworkBehaviour
         }
     }
 
-    private async void HandleLobbyHeartbeat(){
-        if (hostLobby != null)
+    private async void HandleLobbyHeartbeat(){      //로비는 일정시간마다 Heartbeat이라는 신호를 보내주지 않으면 자동으로 삭제됨
+        if (isLobbyHost)
         {
             heartbeatTimer -= Time.deltaTime;
             if (heartbeatTimer < 0f)
@@ -124,12 +127,12 @@ public class LobbyManager : NetworkBehaviour
                 float heartbeatTimerMax = 15;
                 heartbeatTimer = heartbeatTimerMax;
 
-                await LobbyService.Instance.SendHeartbeatPingAsync(hostLobby.Id);
+                await LobbyService.Instance.SendHeartbeatPingAsync(joinedLobby.Id);
             }
         }
     }
 
-    private async void HandleLobbyPollForUpdates() {
+    private async void HandleLobbyPollForUpdates() {    //로비에 접속해 있는 상태면 서버에서 로비의 변동상황(옵션 변동 등)을 계속 초기화 시켜줘야됨
         if (joinedLobby != null)
         {
             lobbyUpdateTimer -= Time.deltaTime;
@@ -141,6 +144,11 @@ public class LobbyManager : NetworkBehaviour
                 Lobby lobby = await LobbyService.Instance.GetLobbyAsync(joinedLobby.Id);
                 joinedLobby = lobby;
 
+                if (joinedLobby.Data["START_GAME"].Value != "0") { //게임 시작
+                    if (!isLobbyHost) {
+                        JoinRelay(joinedLobby.Data["START_GAME"].Value);
+                    }
+                }
 
                 
             }
@@ -155,7 +163,7 @@ public class LobbyManager : NetworkBehaviour
     }
 
 
-    void RoomRefresh(List<Player> players) {                            //방 최신화 하는 함수
+    void RoomRefresh(List<Player> players) {                            //방 UI 최신화 하는 함수
         foreach (Transform child in playerHolder)
         {
             Destroy(child.gameObject);
@@ -173,16 +181,13 @@ public class LobbyManager : NetworkBehaviour
     private async void LeaveLobby() {
         try{
             
-            if (hostLobby != null) {
-                if(joinedLobby.Players.Count >= 2) MigrateLobbyHost();
-            } //만약 호스트 유저라면 변경하고 나가야됨
+            if(joinedLobby.Players.Count >= 2 && isLobbyHost) MigrateLobbyHost();
+            //만약 호스트 유저라면 변경하고 나가야됨
             await LobbyService.Instance.RemovePlayerAsync(joinedLobby.Id, AuthenticationService.Instance.PlayerId);
             roomPanel.SetActive(false);
-            hostLobby = null;
+            isLobbyHost = false;
             joinedLobby = null;
-        }
-        catch (LobbyServiceException e)
-        {
+        } catch (LobbyServiceException e) {
             Debug.Log(e);
         }
     }
@@ -203,20 +208,16 @@ public class LobbyManager : NetworkBehaviour
             callbacks.LobbyChanged += OnLobbyChanged;
             roomPanel.SetActive(true);
             RoomRefresh(joinedLobby.Players);
-        }
-        catch (LobbyServiceException e)
-        {
+        } catch (LobbyServiceException e) {
             Debug.Log(e);
         }
     }
     [Command]
     private async void MigrateLobbyHost() {
         try {
-            hostLobby = await Lobbies.Instance.UpdateLobbyAsync(hostLobby.Id, new UpdateLobbyOptions {
+            joinedLobby = await Lobbies.Instance.UpdateLobbyAsync(joinedLobby.Id, new UpdateLobbyOptions {
                 HostId = joinedLobby.Players[1].Id});
-            joinedLobby = hostLobby;
-        }
-        catch (LobbyServiceException e) {
+        } catch (LobbyServiceException e) {
             Debug.Log(e);
         }
     }
@@ -236,48 +237,47 @@ public class LobbyManager : NetworkBehaviour
             Debug.Log(player.Data["PlayerName"].Value);
         }
     }
-    #endregion
-    #region 4. Relay
 
 
     [Command]
-    private async void StartGameByHost() {
-        try {
-            Allocation allocation = await Relay.Instance.CreateAllocationAsync(2);      //일단 2로
-            string joinCode = await Relay.Instance.GetJoinCodeAsync(allocation.AllocationId);
-            Debug.Log(joinCode);
+    private async void StartGame() {  //클라이언트들이 게임 시작하는 메서드
+        if (isLobbyHost) {
+            try {
+                Allocation allocation = await Relay.Instance.CreateAllocationAsync(2);      //일단 2로
+                string joinCode = await Relay.Instance.GetJoinCodeAsync(allocation.AllocationId);
+                Debug.Log(joinCode);
 
 
-            RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
+                Lobby lobby = await Lobbies.Instance.UpdateLobbyAsync(joinedLobby.Id, new UpdateLobbyOptions { 
+                    Data = new Dictionary<string, DataObject> {
+                        { "START_GAME", new DataObject(DataObject.VisibilityOptions.Member, joinCode) }
+                    }
+                });
+                RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
+                NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
 
-            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+                NetworkManager.Singleton.StartHost();
 
-            NetworkManager.Singleton.StartHost();
-        } catch (RelayServiceException e) {Debug.Log(e);}
-
-        
+            }
+            catch (RelayServiceException e) { Debug.Log(e); }
+        }
     }
+    #endregion
+    #region 4.Relay
     [Command]
-    private async void JoinRelay(string joinCode)
+    private void JoinRelay(string joinCode)
     {
         try
         {
             Debug.Log("Joining Relay with:" + joinCode);
+            /*
             JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
-
             RelayServerData relayServerData = new RelayServerData(joinAllocation, "dtls");
-
             NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
-
             NetworkManager.Singleton.StartClient();
+            */
         }
         catch (RelayServiceException e) { Debug.Log(e); }
-    }
-
-    [Command]
-    [ClientRpc]
-    void testClientRpc(string text) {
-        Debug.Log(text);
     }
     #endregion
 }
